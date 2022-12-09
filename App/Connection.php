d2518d589ceef696ef4b3959af5d648aa0db4f1c
Artur Henrique <artur.hique@gmail.com>
2022-12-09 20:42:41 -0300

    added gitignore

--- App/Connection.php
+++ App/Connection.php
@@ -5,9 +5,13 @@ namespace App;
 class Connection {
     public static function getDb() {
         try {
-            $dsn="mysql:host=localhost;dbname=scandiweb;port=3306;charset=utf8";
-            $user="root";
-            $pass="#hique519849";
+            // $dsn="mysql:host=localhost;dbname=scandiweb;port=3306;charset=utf8";
+            // $user="root";
+            // $pass="#hique519849";
+
+            $dsn="mysql:host=us-cdbr-east-06.cleardb.net;dbname=heroku_ffca86025b0c438;port=3306;charset=utf8";
+            $user="bd5ece3864105b";
+            $pass="f79126db";
 
             $conn = new \PDO($dsn, $user, $pass);
             
