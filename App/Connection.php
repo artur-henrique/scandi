0329133eee843f63dd9e3b951b551b2838a99746
Artur Henrique <artur.hique@gmail.com>
2022-12-09 21:18:51 -0300

    changed: connection configs

--- App/Connection.php
+++ App/Connection.php
@@ -5,10 +5,12 @@ namespace App;
 class Connection {
     public static function getDb() {
         try {
+            // local
             // $dsn="mysql:host=localhost;dbname=scandiweb;port=3306;charset=utf8";
             // $user="root";
             // $pass="#hique519849";
 
+            //production
             $dsn="mysql:host=us-cdbr-east-06.cleardb.net;dbname=heroku_ffca86025b0c438;port=3306;charset=utf8";
             $user="bd5ece3864105b";
             $pass="f79126db";
