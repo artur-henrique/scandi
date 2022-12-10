77c14457a55efd5d0735c4eb5eb2c964fbf0a3d4
Artur Henrique <artur.hique@gmail.com>
2022-12-09 22:46:43 -0300

    changed: headers

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php 
     require_once "../vendor/autoload.php";
-    header('Location: /index.html');
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-    header("Access-Control-Allow-Headers: Content-Type");
+    // header('Location: /index.php');
+    // header("Access-Control-Allow-Origin: *");
+    // header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    // header("Access-Control-Allow-Headers: Content-Type");
     $route = new \App\Route;
 
     
