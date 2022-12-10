1109962c70a3a189abdaa938bc3803a7e3c7b73f
Artur Henrique <artur.hique@gmail.com>
2022-12-09 23:01:42 -0300

    changed: index

--- public/index.php
+++ public/index.php
@@ -1,9 +1,8 @@
 <?php 
     require_once "../vendor/autoload.php";
-    // header('Location: /index.php');
-    // header("Access-Control-Allow-Origin: *");
-    // header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-    // header("Access-Control-Allow-Headers: Content-Type");
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    header("Access-Control-Allow-Headers: Content-Type");
     $route = new \App\Route;
 
     
