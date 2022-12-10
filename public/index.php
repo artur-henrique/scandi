19240de928f453c0474727d6302bf6d9bd9bb1f7
Artur Henrique <artur.hique@gmail.com>
2022-12-09 23:20:35 -0300

    added: require on index

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php 
     require_once "../vendor/autoload.php";
+    require_once("../App/route.php");
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
     header("Access-Control-Allow-Headers: Content-Type");
