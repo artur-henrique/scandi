3af27cec325a8617679d73bad060b02920b079e5
Artur Henrique <artur.hique@gmail.com>
2022-12-09 22:43:30 -0300

    changed: index and Procfile configs

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php 
     require_once "../vendor/autoload.php";
+    header('Location: /index.html');
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
     header("Access-Control-Allow-Headers: Content-Type");
