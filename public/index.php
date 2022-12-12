9a46b27debb1c8ab6322c854d69b8716604e33d0
Artur Henrique <artur.hique@gmail.com>
2022-12-12 18:30:45 -0300

    changed: headers moved to Bootstrap class

--- public/index.php
+++ public/index.php
@@ -1,10 +1,5 @@
 <?php 
     require_once "../vendor/autoload.php";
     require_once("../App/route.php");
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-    header("Access-Control-Allow-Headers: Content-Type");
     $route = new \App\Route;
-
-    
 ?>
